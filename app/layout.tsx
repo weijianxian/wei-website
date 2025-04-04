73e0a4673d257eddf394614b3ece4e8f905ece92
柠檬味氨水 <33548986+weijianxian@users.noreply.github.com>
2025-04-04 21:18:40 +0000

    feat: 添加 Vercel 分析和速度洞察依赖，并在布局中引入

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,6 @@
 import Icon from '@/public/ico/favicon.ico'
+import { Analytics } from '@vercel/analytics/react'
+import { SpeedInsights } from '@vercel/speed-insights/next'
 import type { Metadata } from 'next'
 import { ToastContainer } from 'react-toastify'
 import './globals.css'
@@ -22,6 +24,8 @@ export default function RootLayout({
       <body className="min-h-screen">
         <ToastContainer />
         {children}
+        <Analytics />
+        <SpeedInsights/>
       </body>
     </html>
   )
