beb4891a542d7ed531ddd6a6f756e46509cb2a46
柠檬味氨水 <33548986+weijianxian@users.noreply.github.com>
2025-04-05 20:22:34 +0800

    fix: 修复 SetuCard 组件的 key 属性，确保唯一性

--- components/setu/shower.tsx
+++ components/setu/shower.tsx
@@ -10,7 +10,7 @@ export default function SetuShower({ data }: SetuShowerProps) {
   return (
     <div className="flex flex-wrap justify-center gap-2 py-2">
       {data ? (
-        data.map((item, index) => <SetuCard key={index} {...item} />)
+        data.map(item => <SetuCard key={`${item.pid}_${item.p}`} {...item} />)
       ) : (
         <p className="m-10 text-gray-500">没有数据</p>
       )}
